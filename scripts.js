55a00b3bc4ae9758e771b81799e0f03cdd4e8ec8
Mouhsinou <mamamouhsinou@gmail.com>
2024-09-21 18:59:04 +0100

    MAMA

---
+++ scripts.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('nav ul li a');
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
